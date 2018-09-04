aeeaf0088b331f651a10d363f7df9fd24d65ac34
haidarzxc <haidar.a.alanbari@gmail.com>
2018-09-04 13:00:34 -0400

    initialize-II

---
+++ core/infer.py
@@ -0,0 +1,29 @@
+
+
+import argparse
+import sys
+
+
+# def parse_args():
+#     parser = argparse.ArgumentParser(description='zxc Desc')
+#     parser.add_argument(
+#         '--zxc',
+#         dest='zxc dest',
+#         help='zxc help',
+#         default=None,
+#         type=str
+#     )
+#
+#     if len(sys.argv) == 1:
+#         parser.print_help()
+#         sys.exit(1)
+#     return parser.parse_args()
+
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    # main(parse_args())
+    pass
