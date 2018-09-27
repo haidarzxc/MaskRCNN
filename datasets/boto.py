51af33e5f7d3679e5dcc39dc10a1f6a9d12710ab
haidarzxc <haidar.a.alanbari@gmail.com>
2018-09-26 23:10:01 -0400

    boto

---
+++ datasets/boto.py
@@ -0,0 +1,26 @@
+import boto3
+import botocore
+
+session = boto3.Session(
+    aws_access_key_id="",
+    aws_secret_access_key="",
+)
+
+s3 = session.resource('s3')
+
+
+try:
+    bucket=s3.Bucket("noaa-nexrad-level2")
+    # print(bucket.Object("1991/12/26/KTLX/KTLX19911226_025749.gz"))
+    x=0
+    for object in bucket.objects.all():
+        print(object.key.split("/")[4])
+        if x==4:
+            break
+        x+=1
+
+except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+        print("The object does not exist.")
+    else:
+        raise
