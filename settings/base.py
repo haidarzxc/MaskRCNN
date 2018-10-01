ba677736b9d615011731437e33f3d46394671798
haidarzxc <haidar.a.alanbari@gmail.com>
2018-09-30 20:15:29 -0400

    base.py

--- settings/base.py
+++ settings/base.py
@@ -1,1 +1,11 @@
 # base
+
+# shift units
+HORIZONTAL_SHIFT=1
+VERTICAL_SHIFT=1
+
+# radar 88D_locations URL
+WSR_88D_LOCATIONS='http://apollo.lsc.vsc.edu/classes/remote/lecture_notes/radar/88d/88D_locations.html'
+
+# ncdc stormevents URL
+NCDC_STORMEVENTS='ftp://ftp.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/'
