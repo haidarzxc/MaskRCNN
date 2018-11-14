50671bc7c596fa5859e90ab670308c101ebb94d2
haidarzxc <haidar.a.alanbari@gmail.com>
2018-11-14 00:35:43 -0500

    recycleView

--- ui/Main.py
+++ ui/Main.py
@@ -9,23 +9,54 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.factory import Factory
 from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 import os
 from Table import *
+
+import os, sys, inspect
+import pandas as pd
+parent_directory = os.path.dirname(\
+                    os.path.dirname(\
+                    os.path.abspath(inspect.getfile(inspect.currentframe()))))
+sys.path.insert(0,parent_directory)
+from datasets.NCDC_stormevents_data_loader import load_CSV_file
 
 class Toolbar(BoxLayout):
     pass
 
-class Root(FloatLayout):
+class Root(BoxLayout):
     sm=None
-    storms_list_view = ObjectProperty()
+    items_list = ObjectProperty(None)
+    column_headings = ObjectProperty(None)
+    rv_data = ListProperty([])
     def __init__(self, **kwargs):
         super(Root,self).__init__(**kwargs)
+        self.get_dataframe()
 
+    def get_dataframe(self):
+        storm_cols=['BEGIN_DATE_TIME','END_DATE_TIME','BEGIN_LON','BEGIN_LAT']
+        storms=load_CSV_file('NCDC_stormevents/StormEvents_details-ftp_v1.0_d2017_c20180918.csv')
+        storms_filtered=storms[storm_cols]
+
+        # Extract and create column headings
+        for heading in storms_filtered.columns:
+            self.column_headings.add_widget(Label(text=heading))
+
+        # Extract and create rows
+        data = []
+        for row in storms_filtered.itertuples():
+            for i in range(1, len(row)):
+                data.append([row[i], row[0]])
+        self.rv_data = [{'text': str(x[0]), 'Index': str(x[1]), 'selectable': True} for x in data]
+
+    def get_row(self, instance):
+        print("delete_row:")
+        print("Button: text={0}, index={1}".format(instance.text, instance.index))
+        print(self.rv_data[instance.index])
+        print("Pandas: Index={}".format(self.rv_data[instance.index]['Index']))
 
     def view(self):
-        print("view",self.storms_list_view.adapter.selection)
-
-
+        print("view")
 
 
 class MainApp(App):
--- ui/table.py
+++ ui/table.py
@@ -1,26 +1,35 @@
-from kivy.uix.listview import ListItemButton
+from kivy.uix.label import Label
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.uix.button import Button
+from kivy.properties import BooleanProperty, ListProperty, ObjectProperty
+from kivy.uix.recyclegridlayout import RecycleGridLayout
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 
-import os, sys, inspect
-import pandas as pd
-parent_directory = os.path.dirname(\
-                    os.path.dirname(\
-                    os.path.abspath(inspect.getfile(inspect.currentframe()))))
-sys.path.insert(0,parent_directory)
-from datasets.NCDC_stormevents_data_loader import load_CSV_file
+class SelectableRecycleGridLayout(FocusBehavior, LayoutSelectionBehavior,
+                                  RecycleGridLayout):
+    ''' Adds selection and focus behaviour to the view. '''
 
-class ListButton(ListItemButton):
-    pass
+class SelectableButton(RecycleDataViewBehavior, Button):
+    ''' Add selection support to the Label '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
 
-def data():
-    storm_cols=['BEGIN_DATE_TIME','END_DATE_TIME','BEGIN_LON','BEGIN_LAT']
-    storms=load_CSV_file('NCDC_stormevents/StormEvents_details-ftp_v1.0_d2017_c20180918.csv')
-    storms_header=pd.DataFrame([storm_cols],columns=storm_cols)
-    storms_filtered=storms[storm_cols]
-    storms_header=storms_header.append(storms_filtered, ignore_index=True)
-    storms_header=storms_header.to_string(header=False,
-                        index=False,
-                        index_names=False).split('\n')
-    print(storms_header)
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(SelectableButton, self).refresh_view_attrs(
+            rv, index, data)
 
-    return storms_header
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableButton, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
 
