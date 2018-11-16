7989620073fd0ce94287b53157a51257738fc69e
haidarzxc <haidar.a.alanbari@gmail.com>
2018-11-16 09:46:29 -0500

    graph change

--- ui/Graph.py
+++ ui/Graph.py
@@ -22,13 +22,19 @@ def graph(path):
 # open the file, create the displays and figure
     radar = pyart.io.read_nexrad_archive(path)
     display = pyart.graph.RadarDisplay(radar)
-    fig = plt.figure(figsize=(6, 5))
-
+    fig = plt.figure(figsize=(10, 8))
+    fig.subplots_adjust(hspace=0.4)
+    nplots = 10
+    for snum in range(10):
+        ax = fig.add_subplot(nplots, 4, snum+1)
+        display.plot('reflectivity', 0,
+                     vmin=-32, vmax=64, ax=ax)
+        display.plot_range_ring(radar.range['data'][-1]/1000., ax=ax)
+        display.set_limits(xlim=(-500, 500), ylim=(-500, 500), ax=ax)
     # plot super resolution reflectivity
-    ax = fig.add_subplot(111)
-    display.plot('reflectivity', 0, title='NEXRAD Reflectivity',
-                 vmin=-32, vmax=64, colorbar_label='', ax=ax)
-    display.plot_range_ring(radar.range['data'][-1]/1000., ax=ax)
-    display.set_limits(xlim=(-500, 500), ylim=(-500, 500), ax=ax)
+
+
+    # --------------
+
 
     plt.show()
