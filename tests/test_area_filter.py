051056d1f935bdf79c45e2a719aceca8ee3ad3bf
haidarzxc <haidar.a.alanbari@gmail.com>
2019-01-28 20:10:00 -0500

    area_filter_test

--- tests/test_area_filter.py
+++ tests/test_area_filter.py
@@ -7,31 +7,31 @@ import pandas as pd
 from datasets.area_filter import AreaFilter
 
 class TestAreaFilter(unittest.TestCase):
-    pass
-    # def test_calculate_distance(self):
-    #     instance1=AreaFilter()
-    #     #method signiture x1,x2,y1,y2
-    #     # example (-1, 1) and (3, 4)
-    #     result=instance1.calculate_distance(-1,3,1,4)
-    #     self.assertEqual(result,5)
-    #     self.assertNotEqual(result,75)
-    #
-    #     result1=instance1.calculate_distance(-1,33,1,11)
-    #     self.assertEqual(result1,35.4400902933387)
-    #     self.assertNotEqual(result1,732)
-    #
-    # def iterate_calculateArea(self,row):
-    #     self.assertEqual(row['AREA'],row['CORRECT_AREA'])
-    #
-    # def test_calculate_area(self):
-    #     instance1=AreaFilter()
-    #     test_df=pd.DataFrame()
-    #     test_df['BEGIN_LON']=pd.Series([-5,0])
-    #     test_df['BEGIN_LAT']=pd.Series([0,5])
-    #     test_df['END_LON']=pd.Series([10,20])
-    #     test_df['END_LAT']=pd.Series([15,25])
-    #     test_df['AREA']=pd.Series([])
-    #     test_df['CORRECT_AREA']=pd.Series([75,400])
-    #
-    #     test_df=test_df.apply(lambda x: instance1.calculate_area(x), axis=1)
-    #     test_df.apply(self.iterate_calculateArea,axis=1)
+
+    def test_calculate_distance(self):
+        instance1=AreaFilter()
+        #method signiture x1,x2,y1,y2
+        # example (-1, 1) and (3, 4)
+        result=instance1.calculate_distance(-1,3,1,4)
+        self.assertEqual(result,5)
+        self.assertNotEqual(result,75)
+
+        result1=instance1.calculate_distance(-1,33,1,11)
+        self.assertEqual(result1,35.4400902933387)
+        self.assertNotEqual(result1,732)
+
+    def iterate_calculateArea(self,row):
+        self.assertEqual(row['AREA'],row['CORRECT_AREA'])
+
+    def test_calculate_area(self):
+        instance1=AreaFilter()
+        test_df=pd.DataFrame()
+        test_df['BEGIN_LON']=pd.Series([-5,0])
+        test_df['BEGIN_LAT']=pd.Series([0,5])
+        test_df['END_LON']=pd.Series([10,20])
+        test_df['END_LAT']=pd.Series([15,25])
+        test_df['AREA']=pd.Series([])
+        test_df['CORRECT_AREA']=pd.Series([225,400])
+
+        test_df=test_df.apply(lambda x: instance1.calculate_area(x), axis=1)
+        test_df.apply(self.iterate_calculateArea,axis=1)
